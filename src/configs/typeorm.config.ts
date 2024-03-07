6bcf2f272080f722a6537012a1353f0e1275800a
Han JiHun <hjh_3229@naver.com>
2024-03-07 15:50:39 +0900

    typeorm 수정

--- src/configs/typeorm.config.ts
+++ src/configs/typeorm.config.ts
@@ -1,12 +1,27 @@
-import { TypeOrmModuleOptions } from "@nestjs/typeorm";
+import * as Joi from '@hapi/joi';
 
-export const typeORMConfig: TypeOrmModuleOptions = {
-    type: 'postgres',
-    host: 'localhost',
-    port: 5432,
-    username: 'postgres',
-    password: '0dc95b2c',
-    database: 'dev-camp',
-    entities: [__dirname + '/../**/*.entity.{js,ts}'],
-    synchronize: true
-}
+export const validationSchema = Joi.object({
+    // ENV
+    RUNTIME: Joi.string().valid('local', 'test', 'prod').required(),
+    PORT: Joi.number().required(),
+    SERVICE_NAME: Joi.string().required(),
+
+    // BACKEND
+    BASE_URL: Joi.string().required(),
+
+    // SWAGGER
+    SWAGGER_ID: Joi.string().required(),
+    SWAGGER_PW: Joi.string().required(),
+
+    // AUTH
+    JWT_SECRET: Joi.string().required(),
+    ACCESS_TOKEN_EXPIRY: Joi.string().required(),
+    REFRESH_TOKEN_EXPIRY: Joi.string().required(),
+
+    // DB
+    DB_HOST: Joi.string().required(),
+    DB_PORT: Joi.number().required(),
+    DB_USERNAME: Joi.string().required(),
+    DB_PASSWORD: Joi.string().required(),
+    DB_DATABASE: Joi.string().required(),
+});
