92f34e9391483383dd07089928ea2f97f74aa00a
Han JiHun <hjh_3229@naver.com>
2024-03-10 16:33:29 +0900

    배포 문제 해결 실패

--- src/interceptors/logging.interceptor.ts
+++ src/interceptors/logging.interceptor.ts
@@ -17,7 +17,7 @@ export class LoggingInterceptor implements NestInterceptor {
     constructor(private readonly accessLogRepository: AccessLogRepository) { }
 
     intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
-        const request: Request = context.switchToHttp().getRequest(); // 아래 문제 발생의 원인을 찾기 위해 Request를 ctrl 클릭을 통해 Request를 찾고, 거기에 user?: Express.User 추가
+        const request: Request & { user?: User } = context.switchToHttp().getRequest(); // 아래 문제 해결을  위해 직접적으로 명시해 user를 찾도록 함
         const { ip, method, originalUrl } = request;
         const userAgent = request.headers['user-agent'] || '';
         const user = request.user as User; // AuthGuard 가 이 값을 설정할 것 // request.user에서 에러 메시지 발생 (request에서 user 정보를 찾지 못함)
