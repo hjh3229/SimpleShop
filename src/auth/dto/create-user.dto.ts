64c64f91903d9c32270b1e68b543481da63d58d3
Han JiHun <hjh_3229@naver.com>
2024-03-15 10:31:31 +0900

    회원가입 이메일, 비밀번호, 전화번호 형식 지정

--- src/auth/dto/create-user.dto.ts
+++ src/auth/dto/create-user.dto.ts
@@ -1,9 +1,20 @@
 import { UserRole } from '../entities';
+import { IsEmail, IsString, Matches, MaxLength, MinLength } from 'class-validator'
 
-export type CreateUserDto = {
+export class CreateUserDto {
     name: string;
+
+    @IsEmail()
     email: string;
+
+    @IsString()
+    @MinLength(8)
+    @MaxLength(20)
+    @Matches(/^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,20}$/)
     password: string;
+
+    @IsString()
+    @Matches(/^\d{2,3}-\d{3,4}-\d{4}$/)
     phone: string;
     role: UserRole;
 };
